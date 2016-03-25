f11b9934a5070ef99dad9a6f3a168d0bf7f62760
Christopher Auyeung <chris@winfieldco.com>
2016-03-25 15:33:18 -0400

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import mailqueue
 version = mailqueue.VERSION
 
 setup(name='django-mail-queue',
-      version="2.2.2.1",
+      version="2.2.2.2",
       description="Simple Mail Queuing for Django",
       long_description=open("README.md", "r").read(),
       classifiers=[
